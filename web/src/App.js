9905807b87bd74a1073e16a7da2e5e9fd8c05489
Francois Killeen <killeenfrancois@gmail.com>
2019-12-08 10:43:06 -0500

    Add initial web template using React and Chart.js

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import LineDemo from './LineDemo';
+import './App.css';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+function App() {
+  return (
+    <div style={styles}>
+    <LineDemo/>
+  </div>
+  );
+}
+
+export default App;
