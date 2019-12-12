8de5c989c9dbd2b227f5b3899d35d68388abeb8e
JGB <bourassajulien@gmail.com>
2019-12-11 22:21:50 -0500

    Added a for loop around the ADC acquisition for testing purpose

--- firmware/hello_world/main/hello_world_main.c
+++ firmware/hello_world/main/hello_world_main.c
@@ -40,10 +40,22 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    /* Start a ADC acquisition on GPIO 36 */
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
-    int val = adc1_get_raw(ADC1_CHANNEL_0);
-    printf("The ADC value of GPIO 36: %d\n",val);
+
+    /* Start 1= ADC acquisitions on GPIO 36 */
+    for (int i = 10; i >= 0; i--) {
+        adc1_config_width(ADC_WIDTH_BIT_12);
+        adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
+        int val = adc1_get_raw(ADC1_CHANNEL_0);
+        printf("The ADC value of GPIO 36: %d\n",val);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 
 }
