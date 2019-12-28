db06fa11b335939bb4d1622fe9573156ba095e82
Francois Killeen <killeenfrancois@gmail.com>
2019-12-28 17:04:46 -0500

    Display MQTT data through a streaming chart

--- server/src/moist.js
+++ server/src/moist.js
@@ -1,16 +1,39 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://127.0.0.1')
+var mqttClient = mqtt.connect('mqtt://127.0.0.1')
+mqttClient.subscribe('data');
 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
+const
+    io = require("socket.io"),
+    server = io.listen(8001);
+
+let
+    sequenceNumberByClient = new Map();
+
+mqttClient.on('connect', function () {
+  mqttClient.subscribe('presence', function (err) {
     if (!err) {
-      client.publish('presence', 'Hello mqtt')
+      mqttClient.publish('presence', 'Hello mqtt')
     }
   })
 })
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
+// event fired every time a new client connects:
+server.on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    // initialize this client's sequence number
+    sequenceNumberByClient.set(socket, 1);
+
+    // when socket disconnects, remove it from the list:
+    socket.on("disconnect", () => {
+        sequenceNumberByClient.delete(socket);
+        console.info(`Client gone [id=${socket.id}]`);
+    });
+});
+
+// event fired on received mqtt data
+mqttClient.on('message', function (topic, message) {
+  for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+      console.info(message.toString());
+      client.emit("data", parseInt(message));
+  }
 })
